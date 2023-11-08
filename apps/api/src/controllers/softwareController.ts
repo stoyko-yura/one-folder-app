5c6a949ffecafb0937255bd40d03c701efd3cd26
stoyko-yura <stoyko.yuri@gmail.com>
2023-11-08 22:19:13 +0200

    chore(api): rewrite software controllers and routes for new prisma schema

--- apps/api/src/controllers/softwareController.ts
+++ apps/api/src/controllers/softwareController.ts
@@ -13,15 +13,15 @@ export const getSoftwares = async (req: Request, res: Response) => {
       include: {
         _count: {
           select: {
+            categories: true,
             folders: true,
-            ratings: true,
-            softwareCategories: true
+            ratings: true
           }
         },
-        softwareCategories: true
+        categories: true
       },
       orderBy: {
-        createdAt: 'asc'
+        name: 'asc'
       },
       skip: Number(pageIndex) * (Number(limit) || 10),
       take: Number(limit) || 10
@@ -71,12 +71,12 @@ export const getSoftware = async (req: Request, res: Response) => {
       include: {
         _count: {
           select: {
+            categories: true,
             folders: true,
-            ratings: true,
-            softwareCategories: true
+            ratings: true
           }
         },
-        softwareCategories: true
+        categories: true
       },
       where: {
         id: softwareId
@@ -91,6 +91,224 @@ export const getSoftware = async (req: Request, res: Response) => {
       message: 'Software loaded',
       software,
       success: true
+    });
+  } catch (error) {
+    errorHandler(error as Error, res);
+  }
+};
+
+// Get software folders
+export const getSoftwareFolders = async (req: Request, res: Response) => {
+  try {
+    const { page, limit, pageIndex } = req.query;
+    const { softwareId } = req.params;
+
+    const software = await dbClient.software.findUnique({
+      where: {
+        id: softwareId
+      }
+    });
+
+    if (!software) {
+      return errorHandler(new Error(`Software ${softwareId} not found`), res, 404);
+    }
+
+    const softwareFolders = await dbClient.folder.findMany({
+      orderBy: {
+        createdAt: 'asc'
+      },
+      skip: Number(pageIndex) * (Number(limit) || 10),
+      take: Number(limit) || 10,
+      where: {
+        software: {
+          some: {
+            id: softwareId
+          }
+        }
+      }
+    });
+
+    if (!softwareFolders.length) {
+      return errorHandler(new Error("Software's folders not found"), res, 404);
+    }
+
+    const totalSoftwareFolders = (
+      await dbClient.folder.findMany({
+        where: {
+          software: {
+            some: {
+              id: softwareId
+            }
+          }
+        }
+      })
+    ).length;
+    const totalPages = Math.ceil(totalSoftwareFolders / Number(limit));
+
+    const links = {
+      next:
+        Number(page) < totalPages
+          ? `${req.protocol}://${req.headers.host}/api/software/${softwareId}/folders?page=${
+              Number(page) + 1
+            }&limit=${Number(limit)}`
+          : null,
+      previus:
+        Number(page) > 1
+          ? `${req.protocol}://${req.headers.host}/api/software/${softwareId}/folders?page=${
+              Number(page) - 1
+            }&limit=${Number(limit)}`
+          : null
+    };
+
+    res.status(200).json({
+      links,
+      message: "Software's folders loaded",
+      softwareFolders,
+      success: true,
+      totalPages,
+      totalSoftwareFolders
+    });
+  } catch (error) {
+    errorHandler(error as Error, res);
+  }
+};
+
+// Get software ratings
+export const getSoftwareRatings = async (req: Request, res: Response) => {
+  try {
+    const { page, limit, pageIndex } = req.query;
+    const { softwareId } = req.params;
+
+    const software = await dbClient.software.findUnique({
+      where: {
+        id: softwareId
+      }
+    });
+
+    if (!software) {
+      return errorHandler(new Error(`Software ${softwareId} not found`), res, 404);
+    }
+
+    const softwareRatings = await dbClient.rating.findMany({
+      orderBy: {
+        createdAt: 'asc'
+      },
+      skip: Number(pageIndex) * (Number(limit) || 10),
+      take: Number(limit) || 10,
+      where: {
+        softwareId
+      }
+    });
+
+    if (!softwareRatings.length) {
+      return errorHandler(new Error("Software's ratings not found"), res, 404);
+    }
+
+    const totalSoftwareRatings = (
+      await dbClient.rating.findMany({
+        where: {
+          softwareId
+        }
+      })
+    ).length;
+    const totalPages = Math.ceil(totalSoftwareRatings / Number(limit));
+
+    const links = {
+      next:
+        Number(page) < totalPages
+          ? `${req.protocol}://${req.headers.host}/api/software/${softwareId}/ratings?page=${
+              Number(page) + 1
+            }&limit=${Number(limit)}`
+          : null,
+      previus:
+        Number(page) > 1
+          ? `${req.protocol}://${req.headers.host}/api/software/${softwareId}/ratings?page=${
+              Number(page) - 1
+            }&limit=${Number(limit)}`
+          : null
+    };
+
+    res.status(200).json({
+      links,
+      message: "Software's ratings loaded",
+      softwareRatings,
+      success: true,
+      totalPages,
+      totalSoftwareRatings
+    });
+  } catch (error) {
+    errorHandler(error as Error, res);
+  }
+};
+
+// Get software categories
+export const getSoftwareCategories = async (req: Request, res: Response) => {
+  try {
+    const { page, limit, pageIndex } = req.query;
+    const { softwareId } = req.params;
+
+    const software = await dbClient.software.findUnique({
+      where: {
+        id: softwareId
+      }
+    });
+
+    if (!software) {
+      return errorHandler(new Error(`Software ${softwareId} not found`), res, 404);
+    }
+
+    const softwareCategories = await dbClient.category.findMany({
+      orderBy: {
+        name: 'asc'
+      },
+      skip: Number(pageIndex) * (Number(limit) || 10),
+      take: Number(limit) || 10,
+      where: {
+        software: {
+          some: {
+            id: softwareId
+          }
+        }
+      }
+    });
+
+    if (!softwareCategories.length) {
+      return errorHandler(new Error("Software's categories not found"), res, 404);
+    }
+
+    const totalSoftwareCategories = (
+      await dbClient.category.findMany({
+        where: {
+          software: {
+            some: { id: softwareId }
+          }
+        }
+      })
+    ).length;
+    const totalPages = Math.ceil(totalSoftwareCategories / Number(limit));
+
+    const links = {
+      next:
+        Number(page) < totalPages
+          ? `${req.protocol}://${req.headers.host}/api/software/${softwareId}/categories?page=${
+              Number(page) + 1
+            }&limit=${Number(limit)}`
+          : null,
+      previus:
+        Number(page) > 1
+          ? `${req.protocol}://${req.headers.host}/api/software/${softwareId}/categories?page=${
+              Number(page) - 1
+            }&limit=${Number(limit)}`
+          : null
+    };
+
+    res.status(200).json({
+      links,
+      message: "Software's categories loaded",
+      softwareCategories,
+      success: true,
+      totalPages,
+      totalSoftwareCategories
     });
   } catch (error) {
     errorHandler(error as Error, res);
@@ -123,15 +341,15 @@ export const postSoftware = async (req: Request, res: Response) => {
 
     const createdSoftware = await dbClient.software.create({
       data: {
-        description,
-        icon,
-        name,
-        softwareCategories: {
+        categories: {
           connectOrCreate: categories.map((category: string) => ({
             create: { name: category },
             where: { name: category }
           }))
         },
+        description,
+        icon,
+        name,
         url
       }
     });
@@ -164,15 +382,15 @@ export const putSoftware = async (req: Request, res: Response) => {
 
     const editedSoftware = await dbClient.software.update({
       data: {
-        description,
-        icon,
-        name,
-        softwareCategories: {
+        categories: {
           connectOrCreate: categories.map((category: string) => ({
             create: { name: category },
             where: { name: category }
           }))
         },
+        description,
+        icon,
+        name,
         url
       },
       where: {
--- apps/api/src/routes/softwareRouter.ts
+++ apps/api/src/routes/softwareRouter.ts
@@ -11,6 +11,15 @@ router.get('/', pagination, softwareControllers.getSoftwares);
 // Get software
 router.get('/:softwareId', softwareControllers.getSoftware);
 
+// Get software folders
+router.get('/:softwareId/folders', pagination, softwareControllers.getSoftwareFolders);
+
+// Get software ratings
+router.get('/:softwareId/ratings', pagination, softwareControllers.getSoftwareRatings);
+
+// Get software categories
+router.get('/:softwareId/categories', pagination, softwareControllers.getSoftwareCategories);
+
 // Post software
 router.post('/', softwareControllers.postSoftware);
 
